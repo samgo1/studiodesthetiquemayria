b21dd36165367985ba43a54c0db35c814f0f64c7
Samuel Gosselin <samuelgosselin1@hotmail.com>
2018-06-27 22:44:39 -0400

    ADD ON: pagesController and Bootstrap installation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages_controller/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages_controller#home'
 end
